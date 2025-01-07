e74ed64e67b999f52c5c9e81ed2b2ac93904fd67
KenethRosales <110185247+KenethRosales@users.noreply.github.com>
2025-01-07 16:22:24 -0600

    Shopify theme Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ const cssnano = require('cssnano');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass')(require('sass'));;
-const rename = require('gulp-rename');
+//const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const touch = require('gulp-touch-cmd');
 
 gulp.task('styles', function () {
   const plugins = [
@@ -16,13 +19,16 @@ gulp.task('styles', function () {
   ];
 
   return gulp
-    .src(['./{layout,snippets,templates,assets,sections}/*.{scss,css}', '!./{layout,snippets,templates,assets,sections}/*.min.{scss,css}'])
+    .src('./sass/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(plugins))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./')); 
+    //.pipe(rename({ suffix: '.min' }))
+    .pipe(cleanCSS())
+    .pipe(concat('./css/styles.css'))
+    .pipe(gulp.dest('./assets'))
+    .pipe(touch()); 
 });
-
+/*
 gulp.task('scripts', function () {
   return gulp
   .src(['./{layout,snippets,templates,assets,sections}/*.js', '!./{layout,snippets,templates,assets,sections}/*.min.js'])
@@ -33,10 +39,10 @@ gulp.task('scripts', function () {
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./'));
 });
-
+*/
 gulp.task('watch', function () {
-  gulp.watch(['./{layout,snippets,templates,assets,sections}/*.{scss,css}', '!./{layout,snippets,templates,assets,sections}/*.min.{scss,css}'], gulp.series('styles'));
-  gulp.watch(['./{layout,snippets,templates,assets,sections}/*.js', '!./{layout,snippets,templates,assets,sections}/*.min.js'], gulp.series('scripts'));
+  gulp.watch(['./sass/*.scss'], gulp.series('styles'));
+  //gulp.watch(['./{layout,snippets,templates,assets,sections}/*.js', '!./{layout,snippets,templates,assets,sections}/*.min.js'], gulp.series('scripts'));
 });
 
 gulp.task('default', gulp.series('watch'));
